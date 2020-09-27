4e6592261f4c1f52b6a91040c9be256232dc3a72
julietezekwe <julietezekwe@gmail.com>
2020-09-27 13:31:54 +0100

    add more fields to plot alloction

--- src/components/CreatePlot/index.js
+++ src/components/CreatePlot/index.js
@@ -13,7 +13,18 @@ const CreatePlot = () => {
     <label for="inputAddress2">Plot Number</label>
     <input type="text" className="form-control" id="customerId" placeholder="6788yy"/>
   </div>
-  
+  <div className="form-group">
+    <label for="inputAddress2">Plot Dimension</label>
+    <input type="text" className="form-control" id="customerId" placeholder="6788yy"/>
+  </div>
+  <div className="form-group">
+    <label for="inputAddress2">Estate Name</label>
+    <input type="text" className="form-control" id="customerId" placeholder="6788yy"/>
+  </div>
+  <div className="form-group">
+    <label for="inputAddress2">Estate Address</label>
+    <input type="text" className="form-control" id="customerId" placeholder="6788yy"/>
+  </div>
   <button type="submit" className="btn btn-primary">Allocate</button>
 </form>
     </>
